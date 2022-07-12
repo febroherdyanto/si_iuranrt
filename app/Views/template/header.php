864d5ec4da2ab62786f679cfc4e0f5f4537da28e
Febro Herdyanto <febroherdyanto@outloook.com>
2022-07-12 22:28:20 +0700

    Change ENV

--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -5,7 +5,7 @@
             <div class="col-md-8">
                 <div class="page-header-title">
                     <h5 class="m-b-10"><?= $title; ?></h5>
-                    <p class="m-b-0">Selamat Datang ............ di Sistem Informasi Iuran RT "Siforate FHika"</p>
+                    <p class="m-b-0">Selamat Datang User di Sistem Informasi Iuran RT "Siforate FHika"</p>
                 </div>
             </div>
             <div class="col-md-4">
